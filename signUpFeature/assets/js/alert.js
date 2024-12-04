7e2071f091c238a25a3fe07e3bb4a30fb0aa11f2
Tanya Punater <tanyapunater@tanyas-air.lan>
2024-12-04 17:13:54 -0500

    committing changes for the study page

--- signUpFeature/assets/js/alert.js
+++
@@ -1,5 +0,0 @@
-// function showAlertAndSubmit() {
-//     alert("Sign Up button clicked!");
-//     document.getElementById("signUpForm").submit(); // This submits the form
-// }
-// document.getElementById("signup-button").addEventListener("click", showAlertAndSubmit);
